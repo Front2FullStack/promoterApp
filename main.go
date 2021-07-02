e0b774be793d38a6ff5971e0a3db885a4d1ea3c8
Sushil Parajuli <sushilparajuli2010@gmail.com>
2021-07-02 10:44:12 +0400

    project initialized with gorm and hot reloading with air

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"promoter/src/database"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+
+	database.Connect()
+	database.AutoMigrate()
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World Sushil  👋!")
+	})
+
+	app.Listen(":3000")
+}
---
+++ src/database/db.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"promoter/src/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	var err error
+	DB, err = gorm.Open(mysql.Open("root:root@tcp(db:3306)/promoter"), &gorm.Config{})
+	if err != nil {
+		panic("Could not connect with the database!")
+	}
+}
+
+func AutoMigrate() {
+	DB.AutoMigrate(models.User{})
+}
